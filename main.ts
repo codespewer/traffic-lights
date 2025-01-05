c6633079596017d6b6a5ddd63490946c827f355f
codespewer <161716257+codespewer@users.noreply.github.com>
2025-01-05 17:06:51 +1100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(5000)
+pins.digitalWritePin(DigitalPin.P2, 0)
+pins.digitalWritePin(DigitalPin.P1, 1)
+basic.pause(5000)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P0, 1)
+basic.forever(function () {
+	
+})
