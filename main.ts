2fa4638dfe97b45aa5870ea52baccdb05fa5d93e
codespewer <161716257+codespewer@users.noreply.github.com>
2025-01-05 17:53:01 +1100

    no terning

--- main.ts
+++ main.ts
@@ -1,137 +1,19 @@
-input.onButtonPressed(Button.A, function () {
-    for (let index = 0; index < 15; index++) {
-        basic.showLeds(`
-            . . # . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . # . .
-            # . # . #
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # . # . #
-            . # # # .
-            . . . . .
-            `)
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # . # . #
-            . # # # .
-            . . # . .
-            `)
-    }
-})
-pins.digitalWritePin(DigitalPin.P2, 1)
-basic.pause(5000)
-pins.digitalWritePin(DigitalPin.P2, 0)
-pins.digitalWritePin(DigitalPin.P1, 1)
-basic.pause(2000)
-pins.digitalWritePin(DigitalPin.P1, 0)
-pins.digitalWritePin(DigitalPin.P0, 1)
-basic.pause(3000)
-pins.digitalWritePin(DigitalPin.P0, 0)
-pins.digitalWritePin(DigitalPin.P2, 1)
-basic.pause(5000)
-pins.digitalWritePin(DigitalPin.P2, 1)
-basic.pause(5000)
-pins.digitalWritePin(DigitalPin.P2, 0)
-pins.digitalWritePin(DigitalPin.P1, 1)
-basic.pause(2000)
-pins.digitalWritePin(DigitalPin.P1, 0)
-pins.digitalWritePin(DigitalPin.P0, 1)
-basic.pause(3000)
-pins.digitalWritePin(DigitalPin.P0, 0)
-pins.digitalWritePin(DigitalPin.P2, 1)
-basic.pause(5000)
-pins.digitalWritePin(DigitalPin.P2, 1)
-basic.pause(5000)
-pins.digitalWritePin(DigitalPin.P2, 0)
-pins.digitalWritePin(DigitalPin.P1, 1)
-basic.pause(2000)
-pins.digitalWritePin(DigitalPin.P1, 0)
-pins.digitalWritePin(DigitalPin.P0, 1)
-basic.pause(3000)
-pins.digitalWritePin(DigitalPin.P0, 0)
-pins.digitalWritePin(DigitalPin.P2, 1)
-basic.pause(5000)
-pins.digitalWritePin(DigitalPin.P2, 1)
-basic.pause(5000)
-pins.digitalWritePin(DigitalPin.P2, 0)
-pins.digitalWritePin(DigitalPin.P1, 1)
-basic.pause(2000)
-pins.digitalWritePin(DigitalPin.P1, 0)
-pins.digitalWritePin(DigitalPin.P0, 1)
-basic.pause(3000)
-pins.digitalWritePin(DigitalPin.P0, 0)
-pins.digitalWritePin(DigitalPin.P2, 1)
-basic.pause(5000)
-pins.digitalWritePin(DigitalPin.P2, 1)
-basic.pause(5000)
-pins.digitalWritePin(DigitalPin.P2, 0)
-pins.digitalWritePin(DigitalPin.P1, 1)
-basic.pause(2000)
-pins.digitalWritePin(DigitalPin.P1, 0)
-pins.digitalWritePin(DigitalPin.P0, 1)
-basic.pause(3000)
-pins.digitalWritePin(DigitalPin.P0, 0)
-pins.digitalWritePin(DigitalPin.P2, 1)
-basic.pause(5000)
-pins.digitalWritePin(DigitalPin.P2, 1)
-basic.pause(5000)
-pins.digitalWritePin(DigitalPin.P2, 0)
-pins.digitalWritePin(DigitalPin.P1, 1)
-basic.pause(2000)
-pins.digitalWritePin(DigitalPin.P1, 0)
-pins.digitalWritePin(DigitalPin.P0, 1)
-basic.pause(3000)
-pins.digitalWritePin(DigitalPin.P0, 0)
-pins.digitalWritePin(DigitalPin.P2, 1)
-basic.pause(5000)
-pins.digitalWritePin(DigitalPin.P2, 1)
-basic.pause(5000)
-pins.digitalWritePin(DigitalPin.P2, 0)
-pins.digitalWritePin(DigitalPin.P1, 1)
-basic.pause(2000)
-pins.digitalWritePin(DigitalPin.P1, 0)
-pins.digitalWritePin(DigitalPin.P0, 1)
-basic.pause(3000)
-pins.digitalWritePin(DigitalPin.P0, 0)
-pins.digitalWritePin(DigitalPin.P2, 1)
-basic.pause(5000)
-pins.digitalWritePin(DigitalPin.P2, 1)
-basic.pause(5000)
-pins.digitalWritePin(DigitalPin.P2, 0)
-pins.digitalWritePin(DigitalPin.P1, 1)
-basic.pause(2000)
-pins.digitalWritePin(DigitalPin.P1, 0)
-pins.digitalWritePin(DigitalPin.P0, 1)
-basic.pause(3000)
-pins.digitalWritePin(DigitalPin.P0, 0)
-pins.digitalWritePin(DigitalPin.P2, 1)
-basic.pause(5000)
-basic.forever(function () {
-	
-})
+for (let index = 0; index < 10; index++) {
+    pins.digitalWritePin(DigitalPin.P2, 1)
+    basic.pause(5000)
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(2000)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(3000)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.clearScreen()
+}
