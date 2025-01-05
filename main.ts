9995e07a557a9d77a209d448c19f7d5929c12e3b
codespewer <161716257+codespewer@users.noreply.github.com>
2025-01-05 17:31:59 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,137 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 15; index++) {
+        basic.showLeds(`
+            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            # . # . #
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+})
 pins.digitalWritePin(DigitalPin.P2, 1)
 basic.pause(5000)
 pins.digitalWritePin(DigitalPin.P2, 0)
 pins.digitalWritePin(DigitalPin.P1, 1)
-basic.pause(5000)
+basic.pause(2000)
 pins.digitalWritePin(DigitalPin.P1, 0)
 pins.digitalWritePin(DigitalPin.P0, 1)
+basic.pause(3000)
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(5000)
+pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(5000)
+pins.digitalWritePin(DigitalPin.P2, 0)
+pins.digitalWritePin(DigitalPin.P1, 1)
+basic.pause(2000)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P0, 1)
+basic.pause(3000)
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(5000)
+pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(5000)
+pins.digitalWritePin(DigitalPin.P2, 0)
+pins.digitalWritePin(DigitalPin.P1, 1)
+basic.pause(2000)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P0, 1)
+basic.pause(3000)
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(5000)
+pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(5000)
+pins.digitalWritePin(DigitalPin.P2, 0)
+pins.digitalWritePin(DigitalPin.P1, 1)
+basic.pause(2000)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P0, 1)
+basic.pause(3000)
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(5000)
+pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(5000)
+pins.digitalWritePin(DigitalPin.P2, 0)
+pins.digitalWritePin(DigitalPin.P1, 1)
+basic.pause(2000)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P0, 1)
+basic.pause(3000)
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(5000)
+pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(5000)
+pins.digitalWritePin(DigitalPin.P2, 0)
+pins.digitalWritePin(DigitalPin.P1, 1)
+basic.pause(2000)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P0, 1)
+basic.pause(3000)
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(5000)
+pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(5000)
+pins.digitalWritePin(DigitalPin.P2, 0)
+pins.digitalWritePin(DigitalPin.P1, 1)
+basic.pause(2000)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P0, 1)
+basic.pause(3000)
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(5000)
+pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(5000)
+pins.digitalWritePin(DigitalPin.P2, 0)
+pins.digitalWritePin(DigitalPin.P1, 1)
+basic.pause(2000)
+pins.digitalWritePin(DigitalPin.P1, 0)
+pins.digitalWritePin(DigitalPin.P0, 1)
+basic.pause(3000)
+pins.digitalWritePin(DigitalPin.P0, 0)
+pins.digitalWritePin(DigitalPin.P2, 1)
+basic.pause(5000)
 basic.forever(function () {
 	
 })
