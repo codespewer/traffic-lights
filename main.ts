83f13c6c30d1afa8a148544503d1dcd9bb0561e3
codespewer <161716257+codespewer@users.noreply.github.com>
2025-02-02 13:55:02 +1100

    2 sets with 1 microbit

--- main.ts
+++ main.ts
@@ -23,21 +23,19 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 for (let index = 0; index < 10; index++) {
+    pins.digitalWritePin(DigitalPin.P16, 1)
     pins.digitalWritePin(DigitalPin.P2, 1)
     basic.pause(5000)
+    pins.digitalWritePin(DigitalPin.P16, 0)
     pins.digitalWritePin(DigitalPin.P2, 0)
     pins.digitalWritePin(DigitalPin.P1, 1)
-    basic.pause(2000)
+    pins.digitalWritePin(DigitalPin.P9, 1)
+    basic.pause(5000)
     pins.digitalWritePin(DigitalPin.P1, 0)
+    pins.digitalWritePin(DigitalPin.P9, 0)
     pins.digitalWritePin(DigitalPin.P0, 1)
-    basic.showLeds(`
-        . . # . .
-        . . # . .
-        # . # . #
-        . # # # .
-        . . # . .
-        `)
-    basic.pause(3000)
+    pins.digitalWritePin(DigitalPin.P8, 1)
+    basic.pause(5000)
     pins.digitalWritePin(DigitalPin.P0, 0)
-    basic.clearScreen()
+    pins.digitalWritePin(DigitalPin.P8, 0)
 }
